e6e6ba2b74e66d2594211d13653f9acd5377a8e6
CarlosEReis <carlos.er7@gmail.com>
2025-04-11 21:41:41 -0300

    chore: update API URL to new endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-    //apiUrl: 'https://er7-mybus-api.azurewebsites.net',
-    apiUrl: 'https://er7-my-bus-api.onrender.com',
+    apiUrl: 'https://meuonibus.er7.site',
 };

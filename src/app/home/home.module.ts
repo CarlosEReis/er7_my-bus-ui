298affa62345dd42c15e9fab06d9f6ebacea1190
CarlosEReis <carlos.er7@gmail.com>
2025-06-11 21:40:58 -0300

    feat: cria modulo home e componente home

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,61 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home/home.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { CardModule } from 'primeng/card';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { TagModule } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { KnobModule } from 'primeng/knob';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { DialogModule } from 'primeng/dialog';
+import { SidebarModule } from 'primeng/sidebar';
+import { ImageModule } from 'primeng/image';
+
+import { FormsModule } from '@angular/forms';
+
+import { StyleClassModule } from 'primeng/styleclass';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+
+
+    // BrowserModule,
+    // BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    TagModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    ButtonModule,
+    RadioButtonModule,
+    SelectButtonModule,
+    SpeedDialModule,
+    KnobModule,
+    SidebarModule,
+    DialogModule,
+    ImageModule,
+
+    CardModule,
+    AutoCompleteModule,
+    StyleClassModule,
+  ]
+})
+export class HomeModule { }

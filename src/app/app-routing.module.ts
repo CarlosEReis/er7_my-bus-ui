fb8f4ad5d597a72d1175be977a4406ab0d3f615b
CarlosEReis <carlos.er7@gmail.com>
2025-06-11 21:43:09 -0300

    refactor: update app
    - app-routing
    - app-component
    - app-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule), canActivate: [ AuthGuard ] },
+  { path: 'login', loadChildren: () => import('./security/security.module').then(m => m.SecurityModule) },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { HttpClientModule } from "@angular/common/http";
+import localePt from '@angular/common/locales/pt';
+import { HttpClientModule, provideHttpClient, withInterceptors } from "@angular/common/http";
+
+import { AuthHttpInterceptor, authHttpInterceptorFn, AuthModule, provideAuth0 } from '@auth0/auth0-angular';
+
 
 import { CardModule } from 'primeng/card';
 import { AutoCompleteModule } from 'primeng/autocomplete';
@@ -17,17 +21,20 @@ import { SpeedDialModule } from 'primeng/speeddial';
 import { DialogModule } from 'primeng/dialog';
 import { SidebarModule } from 'primeng/sidebar';
 import { ImageModule } from 'primeng/image';
+import { registerLocaleData } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
 import { StyleClassModule } from 'primeng/styleclass';
+import { environment } from 'src/environments/environment';
 
+const URL_API = environment.apiUrl; // Replace with your actual API URL
+
+registerLocaleData(localePt);
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -48,9 +55,26 @@ import { StyleClassModule } from 'primeng/styleclass';
 
     CardModule,
     AutoCompleteModule,
-    StyleClassModule    
+    StyleClassModule,
+
+    AuthModule.forRoot({
+      domain: 'er7dev.us.auth0.com',
+      clientId: '6be2DTY0RooPavwgE2lX7vpiAVsngeVX',
+      authorizationParams: {
+        audience: 'http://localhost:8080',
+        redirect_uri: window.location.origin,
+      },
+      httpInterceptor: {
+        allowedList: [`${URL_API}/linhas*`, `${URL_API}linhas/*`],
+      },
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    provideHttpClient(),
+    AuthHttpInterceptor,
+    provideHttpClient(withInterceptors([authHttpInterceptorFn])),
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
